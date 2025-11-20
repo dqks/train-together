5940c0c3542c2e366666128483d866d98880744a
Maxim Shtin <maksimshtin@gmail.com>
2025-11-20 09:51:05 +0500

    feat: transitional commit

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes } from 'react-router';
 import { publicRouteConfig }
     from "../../../../shared/config/routeConfig/publicRouteConfig.tsx";
 import { authRouteConfig }
--- src/entities/User/ui/UserCard/UserCard.tsx
+++ src/entities/User/ui/UserCard/UserCard.tsx
@@ -2,7 +2,8 @@ import { classNames } from "../../../../shared/lib/classNames/classNames";
 import cls from "./UserCard.module.scss"
 import userPicture from "../../../../shared/assets/images/userPicture.jpg"
 // import { AppLink } from "../../../../shared/ui/AppLink/AppLink"; TODO
-import { AuthRoutePath } from "../../../../shared/config/routeConfig/authRouteConfig";
+import { AuthRoutePath }
+    from "../../../../shared/config/routeConfig/authRouteConfig";
 import { useNavigate } from "react-router";
 
 interface UserCardProps {
@@ -23,8 +24,8 @@ export const UserCard = ({className} : UserCardProps) => {
             onClick={clickHandler} 
             className={classNames(cls.UserCard, {}, [className])}
         >
-                <img className={cls.picture} src={userPicture} alt="Картинка пользователя" />
-                <p className={cls.userName}>Username</p>
+            <img className={cls.picture} src={userPicture} alt="Картинка пользователя" />
+            <p className={cls.userName}>Username</p>
         </div>
     )
 }
