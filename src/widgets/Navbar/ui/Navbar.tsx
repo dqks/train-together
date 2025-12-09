8ad643f23aeffde2ae6aa900c49885997b85a6bb
Maxim Shtin <maksimshtin@gmail.com>
2025-12-09 20:30:42 +0500

    feat: use local storage to set if navbar is collapsed when app is initialized

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useOpen } from '@/shared/lib/useOpen/useOpen.tsx';
+import { useEffect, useState } from 'react';
 import { ExpandedNavbar } from './ExpandedNavbar/ExpandedNavbar.tsx';
 import { CollapsedNavbar } from './CollapsedNavbar/CollapsedNavbar.tsx';
 import cls from './Navbar.module.scss';
@@ -8,8 +8,27 @@ interface NavbarProps {
     className?: string;
 }
 
+export const LOCAL_STORAGE_COLLAPSED_KEY = 'isCollapsed';
+
 export const Navbar = ({ className } : NavbarProps) => {
-    const [isOpen, openHandler] = useOpen();
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openHandler = () => {
+        setIsOpen((prev) => !prev);
+    };
+
+    useEffect(() => {
+        if (localStorage.getItem(LOCAL_STORAGE_COLLAPSED_KEY) !== null) {
+            setIsOpen(
+                JSON.parse(localStorage.getItem(LOCAL_STORAGE_COLLAPSED_KEY) as string),
+            );
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_COLLAPSED_KEY, isOpen.toString());
+    }, [isOpen]);
+
     return (
         <div className={classNames(cls.Navbar, { [cls.collapsed]: isOpen }, [className])}>
             {
